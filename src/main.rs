ad0cb44988f621eee0b6ce758d6fc69010db91a2
storycraft <storycraft@pancake.sh>
2025-12-28 11:49:01 +0900

    feat: use input, led task. Run usb as main task

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,81 @@
 #![no_main]
 
 mod config;
+mod input;
+mod led;
 mod logger;
 mod usb;
 
-use crate::usb::usb_task;
+use crate::{
+    input::{InputConfig, InputPinout},
+    led::{LedConfig, LedPinout, led_task},
+    usb::usb_task,
+};
 use embassy_executor::Spawner;
 use embassy_rp::{
+    adc::{self, Adc},
     bind_interrupts,
     peripherals::USB,
-    usb::{Driver as UsbDriver, InterruptHandler},
+    usb::Driver as UsbDriver,
 };
 
 use {defmt_rtt as _, panic_halt as _};
 
 bind_interrupts!(struct Irqs {
-    USBCTRL_IRQ => InterruptHandler<USB>;
+    USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<USB>;
+    ADC_IRQ_FIFO => adc::InterruptHandler;
 });
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    let adc = Adc::new(p.ADC, Irqs, adc::Config::default());
     log::info!("System initialized.");
 
     log::info!("Initializing USB driver...");
     let driver = UsbDriver::new(p.USB, Irqs);
     log::info!("USB driver initialized.");
 
-    let usb_task = usb_task(p.PIN_3, driver);
+    log::info!("Initializing USB devices...");
+    let usb_task = usb_task(
+        InputConfig {
+            adc,
+            dma: p.DMA_CH0,
+            pins: InputPinout {
+                button_1: p.PIN_0,
+                button_2: p.PIN_1,
+                button_3: p.PIN_2,
+                button_4: p.PIN_3,
+
+                fx_1: p.PIN_4,
+                fx_2: p.PIN_5,
+
+                start: p.PIN_6,
+
+                knob_left: p.PIN_26,
+                knob_right: p.PIN_27,
+            },
+        },
+        driver,
+    );
     log::info!("USB devices initialized.");
-    _ = spawner.spawn(usb_task);
+
+    log::info!("Initializing LED...");
+    spawner
+        .spawn(led_task(LedConfig {
+            pins: LedPinout {
+                button_1: p.PIN_8,
+                button_2: p.PIN_9,
+                button_3: p.PIN_10,
+                button_4: p.PIN_11,
+                fx_1: p.PIN_12,
+                fx_2: p.PIN_13,
+                start: p.PIN_14,
+            },
+        }))
+        .unwrap();
+    log::info!("LED initialized.");
 
     log::info!("System started.");
+    usb_task.await;
 }
--- src/usb.rs
+++ src/usb.rs
@@ -1,17 +1,15 @@
 use embassy_futures::join::join3;
-use embassy_rp::{
-    Peri, gpio::{Input, Pull}, peripherals::{PIN_3, USB}, usb::Driver as UsbDriver
-};
-use embassy_usb::class::hid::HidWriter;
+use embassy_rp::{peripherals::USB, usb::Driver as UsbDriver};
 
-use crate::{config, logger::logger_task, usb::hid::GamepadInputReport};
+use crate::{
+    config,
+    input::{InputConfig, input_task},
+    logger::logger_task,
+};
 
 pub mod hid;
 
-#[embassy_executor::task]
-pub async fn usb_task(pin: Peri<'static, PIN_3>, driver: UsbDriver<'static, USB>) {
-    log::info!("Initializing USB devices...");
-
+pub async fn usb_task(input_config: InputConfig, driver: UsbDriver<'static, USB>) {
     // Allocates descriptor and control buffer
     let mut config_descriptor = [0; 256];
     let mut bos_descriptor = [0; 256];
@@ -31,31 +29,12 @@ pub async fn usb_task(pin: Peri<'static, PIN_3>, driver: UsbDriver<'static, USB>
         &mut control_buf,
     );
 
-    // placeholder
-    let gamepad_task = {
-        let mut writer =
-            HidWriter::<_, 8>::new(&mut builder, &mut hid_state, config::usb_gamepad_config());
-
-        async move {
-            let button = Input::new(pin, Pull::Up);
-            loop {
-                let report = GamepadInputReport {
-                    buttons_0: 0,
-                    buttons_1: 0,
-                    dpad: if button.is_low() { 1 } else { 0 },
-                };
-
-                match writer.write_serialize(&report).await {
-                    Ok(()) => {}
-                    Err(e) => log::error!("Failed to send report: {:?}", e),
-                };
-            }
-        }
-    };
+    // Setup HID input task
+    let input_task = input_task(input_config, &mut hid_state, &mut builder);
 
     // Setup logger task
     let logger_task = logger_task(&mut serial_state, &mut builder);
     let mut device = builder.build();
 
-    join3(device.run(), logger_task, gamepad_task).await;
+    join3(device.run(), logger_task, input_task).await;
 }
