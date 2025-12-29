d8c6576197ec32255d92107cfec3a3f90000ae11
storycraft <storycraft@pancake.sh>
2025-12-29 16:43:51 +0900

    refactor: cleanup code, remove reset

--- src/userdata/io.rs
+++ src/userdata/io.rs
@@ -27,32 +27,18 @@ fn userdata_start_offset() -> usize {
     userdata_start() - FLASH_BASE as usize
 }
 
-#[inline(always)]
-/// Size of USERDATA memory
-fn userdata_size() -> usize {
-    unsafe extern "C" {
-        // Linker defined symbol
-        static __userdata_size: u8;
-    }
+use crate::userdata::*;
 
-    &raw const __userdata_size as usize
+#[repr(C, align(4))]
+struct Aligned<T>(T);
+
+#[repr(C)]
+/// Layout reprentation in USERDATA flash
+struct UserdataLayout {
+    pub signature: Aligned<Signature>,
+    pub userdata: Aligned<Userdata>,
+    pub _end: Infallible,
 }
-
-mod layout {
-    use crate::userdata::*;
-
-    #[repr(C, align(4))]
-    struct Aligned<T>(T);
-
-    #[repr(C)]
-    /// Layout reprentation in USERDATA flash
-    pub struct UserdataLayout {
-        pub signature: Aligned<Signature>,
-        pub userdata: Aligned<Userdata>,
-        pub _end: Infallible,
-    }
-}
-use layout::UserdataLayout;
 
 pub struct UserdataIo<'a> {
     flash: Flash<'a, FLASH, Async, { 2 * 1024 * 1024 }>,
@@ -81,54 +67,55 @@ impl<'a> UserdataIo<'a> {
     /// Perform initialization.
     pub async fn init(&mut self) -> Result<Userdata, flash::Error> {
         if !self.is_valid().await? {
-            return self.reset();
+            log::info!("Userdata is invalid. Performing initialization.");
+            let userdata = Userdata::default();
+            self.save(&userdata).await?;
+            return Ok(userdata);
         }
 
         match self.read().await {
-            Ok(data) => Ok(data),
+            Some(data) => Ok(data),
             // Saved data is invalid or failed to read
-            Err(_) => return self.reset(),
+            None => {
+                let userdata = Userdata::default();
+                self.save(&userdata).await?;
+                Ok(userdata)
+            }
         }
     }
 
-    /// Reset [`UserData`] and rewrite signature
-    pub fn reset(&mut self) -> Result<Userdata, flash::Error> {
-        let userdata = Userdata::default();
-        // Save userdata first
-        self.flash.blocking_write(
-            userdata_offset(offset_of!(UserdataLayout, userdata)),
-            userdata.as_bytes(),
-        )?;
-
-        // Set signature
-        self.flash.blocking_write(
-            userdata_offset(offset_of!(UserdataLayout, signature)),
-            Signature::CURRENT.as_bytes(),
-        )?;
-
-        Ok(userdata)
-    }
-
-    pub async fn read(&mut self) -> Result<Userdata, flash::Error> {
+    pub async fn read(&mut self) -> Option<Userdata> {
         let mut buf = [0_u8; { size_of::<Userdata>() }];
         self.flash
             .read(
                 userdata_offset(offset_of!(UserdataLayout, userdata)),
                 &mut buf,
             )
-            .await?;
+            .await
+            .ok()?;
 
-        Ok(Userdata::try_read_from_bytes(&buf).unwrap())
+        Userdata::try_read_from_bytes(&buf).ok()
     }
 
     /// Save [`UserData`].
     /// In case of poweroff, the data may become invalid.
     /// Invalid data will be checked on startup and will be resetted.
-    pub fn save(&mut self, data: &Userdata) -> Result<(), flash::Error> {
-        self.flash.blocking_write(
-            userdata_offset(offset_of!(UserdataLayout, userdata)),
-            data.as_bytes(),
-        )?;
+    pub async fn save(&mut self, data: &Userdata) -> Result<(), flash::Error> {
+        // Write only if changed
+        if self.read().await.is_none_or(|read| read.ne(data)) {
+            self.flash.blocking_write(
+                userdata_offset(offset_of!(UserdataLayout, userdata)),
+                data.as_bytes(),
+            )?;
+        }
+
+        if !self.is_valid().await? {
+            // Set signature
+            self.flash.blocking_write(
+                userdata_offset(offset_of!(UserdataLayout, signature)),
+                Signature::CURRENT.as_bytes(),
+            )?;
+        }
 
         Ok(())
     }
