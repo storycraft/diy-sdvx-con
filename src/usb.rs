9c438d0ef0bd526a8b55ba7a8d1e8f0f6f0fd40c
storycraft <storycraft@pancake.sh>
2025-12-30 17:58:05 +0900

    feat: add more states

--- src/usb.rs
+++ src/usb.rs
@@ -18,7 +18,7 @@ pub async fn usb_task(input_config: InputConfig, driver: UsbDriver<'static, USB>
     let mut control_buf = [0; { config::DEVICE.max_packet_size_0 as usize }];
 
     // Setup function class states
-    let mut hid_state = embassy_usb::class::hid::State::new();
+    let mut hid_states = [const { embassy_usb::class::hid::State::new() }; 4];
     let mut via_state = embassy_usb::class::hid::State::new();
     let mut serial_state = embassy_usb::class::cdc_acm::State::new();
 
@@ -32,7 +32,7 @@ pub async fn usb_task(input_config: InputConfig, driver: UsbDriver<'static, USB>
     );
 
     // Setup HID input task
-    let input_task = input_task(input_config, &mut hid_state, &mut builder);
+    let input_task = input_task(input_config, &mut hid_states, &mut builder);
 
     // Setup via task
     let via_task = via_task(&mut via_state, &mut builder);
